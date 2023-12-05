b77e97cbbb4f64139da27c7f6208b9179417e684
Pete Kayuha <zzz8910@gmail.com>
2023-12-04 21:33:13 -0500

    Day 4 - part 1 is correct. part 2 is not, need to find a way to handle copies of cards

---
+++ Day4/Program.cs
@@ -0,0 +1,77 @@
+﻿
+using System.Text.RegularExpressions;
+
+internal partial class Program
+{
+    private static int CalculateWinnings(string line)
+    {
+        int points = 0;
+
+        var splitLine = line.Split(": ");
+        splitLine = splitLine[1].Trim().Split(" | ");
+
+        var winningNums = MyRegex().Matches(splitLine[0]).ToList();
+        var nums = MyRegex().Matches(splitLine[1]).ToList();
+
+        var matches = nums.Where(n => winningNums.Any(w => w.Value.Equals(n.Value))).ToList();
+
+        if (matches.Count > 0)
+        {
+            points = 1;
+            for (int i = 1; i < matches.Count; i++) //there has to be a better way
+            {
+                points *= 2;
+            }
+        }
+
+        return points;
+    }
+
+    private static void CountCards(List<string> lines)
+    {
+
+        var copiedList = new List<string>();
+
+        for (int i = 0; i < lines.Count; i++)
+        {
+            var splitLine = lines[i].Split(": ");
+            splitLine = splitLine[1].Trim().Split(" | ");
+
+            var winningNums = MyRegex().Matches(splitLine[0]).ToList();
+            var nums = MyRegex().Matches(splitLine[1]).ToList();
+
+            var matches = nums.Where(n => winningNums.Any(w => w.Value.Equals(n.Value))).ToList();
+
+            if (matches.Count > 0)
+            {
+                var range = lines.GetRange(i + 1, matches.Count);
+                copiedList.AddRange(range);
+                lines.AddRange(range);
+            }
+        }
+
+        lines.ForEach(l => Console.WriteLine(l));
+        Console.WriteLine(lines.Count);
+        Console.WriteLine(copiedList.Count);
+    }
+
+    private static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("input.txt");
+        int points = 0;
+
+        CountCards(input.ToList());
+
+        foreach (var line in input)
+        {
+            points += CalculateWinnings(line);
+        }
+
+        // Console.WriteLine(points);
+    }
+
+
+
+    [GeneratedRegex(@"\d+")]
+    private static partial Regex MyRegex();
+}

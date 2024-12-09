80cca93898e6677af0088687fdbe7a7042d69f71
Pete Kayuha <zzz8910@gmail.com>
2024-12-08 21:05:24 -0500

    Day 4 part 2. still incorrect, too low.

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -80,12 +80,12 @@ internal class Program
                         matches++;
                     }
                 }
-                catch (IndexOutOfRangeException) {}
+                catch (IndexOutOfRangeException) { break; }
             }
-            }
-
-            return matches;
         }
+
+        return matches;
+    }
 
     private static void PartOne(string[] data)
     {
@@ -101,9 +101,67 @@ internal class Program
         Console.WriteLine(occurrences);
     }
 
+    private static void PartTwo(string[] data)
+    {
+        int occurrences = 0;
+        var patterns = new List<string> { "M.S", "S.M", "M.(?=M)", "S.(?=S)" };
+        
+        //works with sample set. too low with actual set
+
+        for (int i = 0; i + 2 < data.Length; i++)
+        {            
+            foreach (string pattern in patterns)
+            {
+                var matches = Regex.Matches(data[i], pattern);
+                int startPoint = 0;
+
+                foreach (Match match in matches)
+                {
+                    string value = match.Value;
+
+                    if (pattern.Equals("S.(?=S)"))
+                        value += "S";
+                    else if (pattern.Equals("M.(?=M)"))
+                        value += "M";
+
+                    //need to start at next index and keep original string intact
+                    int j = data[i].IndexOf(value, startPoint);
+                    startPoint = j + 1;
+
+                    if (data[i + 1][j + 1] == 'A' && j + 2 < data[i].Length)
+                    {
+                        if (value.StartsWith('M') && value.ToCharArray()[2] == 'S'
+                        && data[i + 2][j] == 'M' && data[i + 2][j + 2] == 'S')
+                        {
+                            occurrences++;
+                        }
+                        else if (value.StartsWith('S') && value.ToCharArray()[2] == 'S'
+                        && data[i + 2][j] == 'M' && data[i + 2][j + 2] == 'M')
+                        {
+                            occurrences++;
+                        }
+                        else if (value.StartsWith('S') && value.ToCharArray()[2] == 'M'
+                        && data[i + 2][j] == 'S' && data[i + 2][j + 2] == 'M')
+                        {
+                            occurrences++;
+                        }
+                        else if (value.StartsWith('M') && value.ToCharArray()[2] == 'M'
+                        && data[i + 2][j] == 'S' && data[i + 2][j + 2] == 'S')
+                        {
+                            occurrences++;
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("" + occurrences);
+    }
+
     private static void Main(string[] args)
     {
         var input = File.ReadAllLines("/home/zzz711/Documents/AdventOfCode/Day4/input.txt");
-        PartOne(input);
+        //PartOne(input);
+        PartTwo(input);
     }
 }

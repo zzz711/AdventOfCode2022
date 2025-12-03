527228817098d9d88226f5671c1aa276af5e0e96
Pete Kayuha <zzz8910@gmail.com>
2025-12-02 20:06:11 -0500

    2025 - Day 2 brute force solutions to both puzzles

---
+++ 2025/Day2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+var data = File.ReadAllText("/home/zzz711/Documents/AdventOfCode/2025/Day2/input.txt");
+
+List<string> idRanges = [.. data.Split(',')];
+
+#region part1
+
+List<long> invalidIds = [];
+
+foreach (string range in idRanges)
+{
+    long start = long.Parse(range.Split('-')[0]);
+    long end = long.Parse(range.Split('-')[1]);
+
+    for (long i = start; i <= end; i++)
+    {
+        string idStr = i.ToString();
+
+        if (idStr.Length % 2 != 0)
+            continue;    
+
+        if(idStr[..(idStr.Length / 2)].Equals(idStr[(idStr.Length/2)..]))
+            invalidIds.Add(i);
+    }
+}
+
+//Console.WriteLine(invalidIds.Sum());
+
+#endregion
+
+#region part2
+
+invalidIds.Clear();
+
+string pattern = @"^(\d+)\1+$";
+Regex regex = new(pattern);
+
+foreach (string range in idRanges)
+{
+    long start = long.Parse(range.Split('-')[0]);
+    long end = long.Parse(range.Split('-')[1]);
+
+    for (long i = start; i <= end; i++)
+    {
+        string idStr = i.ToString();
+
+        if( regex.Match(idStr).Success)
+            invalidIds.Add(i);
+    }    
+}
+
+Console.WriteLine(invalidIds.Sum());
+
+#endregion
